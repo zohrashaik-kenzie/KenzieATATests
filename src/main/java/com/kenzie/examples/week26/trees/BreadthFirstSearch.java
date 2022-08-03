44dd2ba46b66e61284541223d40a9330eafc9f07
zohrashaik-kenzie <zohra.shaik@kenzie.academy>
2022-08-03 00:37:32 -0500

    Week 26 - Trees

--- src/main/java/com/kenzie/examples/week26/trees/BreadthFirstSearch.java
+++ src/main/java/com/kenzie/examples/week26/trees/BreadthFirstSearch.java
@@ -27,6 +27,22 @@ public class BreadthFirstSearch {
     }
 
     public static void main(String[] args) {
+        TreeNode<String> rootNode = new TreeNode<>("1");
+        TreeNode<String> firstNode = new TreeNode<>("8", rootNode);
+        TreeNode<String> secondNode = new TreeNode<>("5", rootNode);
+        TreeNode<String> thirdNode = new TreeNode<>("2", rootNode);
+
+        TreeNode<String> fourthNode = new TreeNode<>("6", firstNode);
+        TreeNode<String> fifthNode = new TreeNode<>("4", firstNode);
+        TreeNode<String> sixthNode = new TreeNode<>("3", firstNode);
+
+        TreeNode<String> seventhNode = new TreeNode<>("2", thirdNode);
+
+        TreeNode<String> eigthNode = new TreeNode<>("10", fourthNode);
+        TreeNode<String> ninthNode = new TreeNode<>("7", fourthNode);
+
+        boolean found = traverseBFS(rootNode, "12");
+        System.out.println("Found = " + found);
 
     }
 
